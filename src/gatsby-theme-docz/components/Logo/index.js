17048c8527d1680d9055ccd5604652a259b4df40
mehtaishita <ishitamehta33@outlook.com>
2022-03-18 11:45:10 -0500

    restoring logo image

---
+++ src/gatsby-theme-docz/components/Logo/index.js
@@ -0,0 +1,5 @@
+// src/gatsby-theme-docz/components/Logo/index.js
+import React from "react";
+import logo from "./DSNP_Logo.png";
+
+export const Logo = () => <img src={logo} alt="DSNP.org" height={55} width={123} />
